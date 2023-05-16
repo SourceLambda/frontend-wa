04a485d33dc1a7e08be88689709e1bf9e3fcb795
Camilo <52603058+ctatis1@users.noreply.github.com>
2023-05-15 23:10:56 -0500

    cambio de iconos

--- src/pages/Profile/ProfileAddresses.jsx
+++ src/pages/Profile/ProfileAddresses.jsx
@@ -3,9 +3,6 @@ import { deleteAddress, getAddressesByProfile } from "../../util/profileMSQuerie
 import GraphQLQuery from "../../util/graphQLQuery";
 import { Link, Route } from "react-router-dom";
 import { Avatar, Box, Button, ButtonBase, ButtonGroup, Card, CardHeader, Container, IconButton, Menu, MenuItem, Modal, Typography } from "@mui/material";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const ProfileAddresses = ({profile}) => {
     const [addresses, setAddresses] = useState([]);
@@ -77,13 +74,9 @@ const ProfileAddresses = ({profile}) => {
                             }
                             action={
                                 <IconButton aria-label="settings">
-                                    <MoreVertIcon 
-                                        id="basic-button"
-                                        aria-controls={open ? 'basic-menu' : undefined}
-                                        aria-haspopup="true"
-                                        aria-expanded={open ? 'true' : undefined}
-                                        onClick={handleClick}
-                                    />
+                                    <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
                                     <Menu
                                         id="basic-menu"
                                         anchorEl={anchorEl}
@@ -94,11 +87,15 @@ const ProfileAddresses = ({profile}) => {
                                         }}
                                     >
                                         <MenuItem onClick={handleClose}>  
-                                            <EditIcon />
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                            </svg>
                                             <Link to={`/profile/addresses/edit-address/${addr.idAddress}`}>Editar</Link>
                                         </MenuItem>
                                         <MenuItem onClick={handleClose}>
-                                            <DeleteIcon />
+                                            <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                            </svg>
                                             <Link to={`/profile/addresses`} onClick={(e) => {
                                                 handleClose();
                                                 handleOpenModal();
